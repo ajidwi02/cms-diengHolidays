3852e0435f4e18db44c780002e6c48742a396d49
Aji Dwi Prakoso <ajidwi111202@gmail.com>
2025-10-13 11:35:25 +0700

    feat: adding navbar desktop & mobile page

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,7 +1,16 @@
+import { Header } from "../../../components";
 import React from "react";
 
 const AllUsers = () => {
-  return <div>AllUsers</div>;
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title="Trips Page"
+        description="Check out our users in real time"
+      />
+      All Users Page Contents
+    </main>
+  );
 };
 
 export default AllUsers;
